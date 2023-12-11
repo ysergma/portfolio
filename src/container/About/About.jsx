a2edd50a50074746fc80991059f3fa3e39f6ccf1
ysergma <sil3nthilldz@gmail.com>
2023-12-11 22:18:15 +0100

    Add styling and responsive to  the header section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import {motion} from "framer-motion"
 import "./About.scss" 
-
+const abouts = [
+  {title:'Web Development', description:'I am a good Frontend Web Developer',imgUrl:'' },
+  {title:'Web Design', description:'I am a good Frontend Web Designer',imgUrl:'' },
+  {title:'UI/UX ', description:'I am a good Frontend Web Developer',imgUrl:'' }
+]
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'>
+    I know that
+    <span>Good Design</span>
+    <br />
+    means
+    <span>Good Business</span>
+    </h2>
+
+    <div className='app__profiles'>
+      {abouts.map((about,index)=>(
+        <motion.div
+          whileInView={{opacity:1}}
+          whileHover={{scale:1.1}}
+          transition={{duration:0.5, type:"tween"}}
+          className='app__profile-item'
+        >
+
+        </motion.div>
+      ))}
+    </div>
+    </>
   )
 }
 
